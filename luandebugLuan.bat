4564aab7fa548e8d3c3136ac1448e95d7a6f2a82
vmluan <vominhluan88@gmail.com>
2015-09-06 22:27:11 +0700

    test
    test

---
+++ luandebugLuan.bat
@@ -0,0 +1,8 @@
+set CATALINA_HOME=E:\ALUAN\Part_time\apache-tomcat-6.0.35
+set JPDA_ADDRESS=8000
+
+set JPDA_TRANSPORT=dt_socket
+
+set JPDA_SUSPEND=y
+
+E:\ALUAN\Part_time\apache-tomcat-6.0.35\bin\catalina.bat jpda start
