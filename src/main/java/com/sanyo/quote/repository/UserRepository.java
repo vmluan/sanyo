6132b286ad64d39b2b31916a52b31e4ffef29649
vmluan <vominhluan88@gmail.com>
2015-09-06 23:02:35 +0700

    add
    add

--- src/main/java/com/sanyo/quote/repository/UserRepository.java
+++ src/main/java/com/sanyo/quote/repository/UserRepository.java
@@ -1,5 +1,7 @@
 package com.sanyo.quote.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
@@ -7,7 +9,8 @@ import org.springframework.data.repository.query.Param;
 import com.sanyo.quote.domain.User;
 
 public interface UserRepository extends PagingAndSortingRepository<User, Integer> {
-//	@Query("select c from User c where c.username  = :userName")
-//	User findByUsername(@Param("userName")  String username);
-	User findByUsername(String username);
+	@Query("select c from User c where c.username  = :userName")
+	User findByUsername(@Param("userName")  String username);
+	
+	List<User> findByEmail(String username);
 }
