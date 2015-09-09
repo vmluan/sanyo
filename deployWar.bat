a40be1461041d14312ac0257e26d779aab7b26b5
vmluan <vominhluan88@gmail.com>
2015-09-09 20:25:49 +0700

    update new layout

--- deployWar.bat
+++ deployWar.bat
@@ -1,7 +1,7 @@
 call %TOMCAT_FOLDER%\bin\shutdown.bat
 set CATALINA_HOME=E:\ALUAN\Part_time\apache-tomcat-6.0.35
 set TOMCAT_FOLDER=E:\ALUAN\Part_time\apache-tomcat-6.0.35
-set WAR_TARGET="E:\ALUAN\Part_time\sanyo\source\target\ROOT.war"
+set WAR_TARGET="E:\ALUAN\Part_time\sanyo\sanyo\target\ROOT.war"
 set ZIP_EXE="C:\Program Files\WinRAR\WinRAR.exe"
 RMDIR /S /Q %TOMCAT_FOLDER%\webapps\ROOT
 del %TOMCAT_FOLDER%\webapps\ROOT.war
