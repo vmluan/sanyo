6f691b7158b27a9bfd6b68a3cc08457deaeadec6
vmluan <vominhluan88@gmail.com>
2015-09-16 00:14:10 +0700

    update code for Role pages.

---
+++ src/main/java/com/sanyo/quote/web/controller/admin/RoleController.java
@@ -0,0 +1,122 @@
+package com.sanyo.quote.web.controller.admin;
+
+import java.util.List;
+import java.util.Locale;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
+
+import org.apache.commons.codec.digest.DigestUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.sanyo.quote.domain.Group;
+import com.sanyo.quote.domain.User;
+import com.sanyo.quote.helper.ImageHelper;
+import com.sanyo.quote.helper.Utilities;
+import com.sanyo.quote.service.GroupService;
+import com.sanyo.quote.web.form.Message;
+import com.sanyo.quote.web.util.UrlUtil;
+
+@RequestMapping("/admin/roles")
+@Controller
+public class RoleController {
+	final Logger logger = LoggerFactory.getLogger(RoleController.class);
+	@Autowired
+	MessageSource messageSource;
+	@Autowired
+	GroupService groupService;
+	
+	//handle /admin/roles
+	@RequestMapping(method = RequestMethod.GET)
+	public String list(Model uiModel) {
+		logger.info("Listing roles");
+
+		List<Group> groups = groupService.findAll();
+		uiModel.addAttribute("groups", groups);
+		return "roles/list";
+	}
+	
+	// handle screen for updating. Just display form for form edit.
+	@RequestMapping(value = "/{id}", params = "form", method = RequestMethod.GET)
+    public String updateForm(@PathVariable("id") Integer id, Model uiModel) {
+		Group group = groupService.findById(id);
+        uiModel.addAttribute("group", group);
+        return "roles/update";
+	}
+	
+	@RequestMapping(params = "form", method = RequestMethod.GET)
+    public String createForm(Model uiModel) {
+		Group group = new Group();
+        uiModel.addAttribute("group", group);
+        return "roles/create";
+	}
+	//create new role, save to database
+	@RequestMapping(params = "form", method = RequestMethod.POST)
+    public String create(@Valid Group group, BindingResult bindingResult, Model uiModel, 
+    		HttpServletRequest httpServletRequest, RedirectAttributes redirectAttributes, Locale locale) {
+		logger.info("Creating role");
+        if (bindingResult.hasErrors()) {
+			uiModel.addAttribute("message", new Message("error", messageSource.getMessage("role_save_fail", new Object[]{}, locale)));
+            uiModel.addAttribute("group", group);
+            return "roles/create";
+        }
+        uiModel.asMap().clear();
+        redirectAttributes.addFlashAttribute("message", new Message("success", messageSource.getMessage("role_save_success", new Object[]{}, locale)));
+        groupService.save(group);
+        return "redirect:/admin/roles/" + UrlUtil.encodeUrlPathSegment(group.getGroupid().toString(), httpServletRequest) + "?form";
+    }
+	
+	//update an existing group, save to database
+	@RequestMapping(value = "/{id}", params = "form", method = RequestMethod.POST)
+	@Transactional
+    public String update(@ModelAttribute("group") Group group, @PathVariable Integer id, Model uiModel, 
+    		HttpServletRequest httpServletRequest, RedirectAttributes redirectAttributes, Locale locale) {
+		logger.info("Updating role");
+        uiModel.asMap().clear();
+        group.setGroupid(id);
+        
+        redirectAttributes.addFlashAttribute("message", new Message("success", messageSource.getMessage("role_save_success", new Object[]{}, locale)));        
+        uiModel.addAttribute("group", group);
+        groupService.save(group);
+        return "roles/update";
+    }
+	
+	@RequestMapping(value = "/getListJson", method = RequestMethod.GET, produces = "application/json; charset=utf-8")
+	@ResponseBody
+	public String getProductsJson(@RequestParam(value="filterscount", required=false) String filterscount
+			, @RequestParam(value="groupscount", required=false) String groupscount
+			, @RequestParam(value="pagenum", required=false) Integer pagenum
+			, @RequestParam(value="pagesize", required=false) Integer pagesize
+			, @RequestParam(value="recordstartindex", required=false) Integer recordstartindex
+			, @RequestParam(value="recordendindex", required=false) Integer recordendindex
+			, HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse){
+		
+		
+		// Constructs page request for current page
+		PageRequest pageRequest = null;
+		pageRequest = new PageRequest(pagenum, pagesize);
+
+		
+		List<Group> groups  = groupService.findAll();
+		String result = Utilities.jSonSerialization(groups);
+		return result;
+	}	
+	
+
+}
---
+++ src/main/webapp/scripts/roleList.js
@@ -0,0 +1,161 @@
+/**
+ * To display jqxgrid in user list page.
+ */
+var url = "roles/getListJson";
+// prepare the data
+var source = {
+	datatype : "json",
+	datafields : [ {
+		name : 'groupName',
+		type : 'string'
+	}, {
+		name : 'description',
+		type : 'string'
+	}, {
+		name : 'viewuser',
+		type : 'bol'
+	}, {
+		name : 'edituser',
+		type : 'bol'
+	}, {
+		name : 'adduser',
+		type : 'bol'
+	}, {
+		name : 'viewgroup',
+		type : 'bol'
+	}, {
+		name : 'editgroup',
+		type : 'bol'
+	}, {
+		name : 'addgroup',
+		type : 'bol'
+	}, {
+		name : 'loginadministrator',
+		type : 'bol'
+	}, {
+		name : 'groupid',
+		type : 'string'
+	}
+	],
+	id : 'groupid',
+	url : url
+};
+var cellsrenderer = function(row, columnfield, value, defaulthtml, columnproperties, rowdata) {
+    if (value < 20) {
+        return '<span style="margin: 4px; float: ' + columnproperties.cellsalign + '; color: #ff0000;">' + value + '</span>';
+    }
+    else {
+        return '<span style="margin: 4px; float: ' + columnproperties.cellsalign + '; color: #008000;">' + value + '</span>';
+    }
+}
+var dataAdapter = new $.jqx.dataAdapter(source, {
+	downloadComplete : function(data, status, xhr) {
+	},
+	loadComplete : function(data) {
+	},
+	loadError : function(xhr, status, error) {
+	}
+});
+// initialize jqxGrid
+$("#list")
+		.jqxGrid(
+				{
+					width : '100%',
+					height : 500,
+					source : dataAdapter,
+					sortable : true,
+					pageable : true,
+					autoheight : true,
+					autoloadstate : false,
+					autosavestate : false,
+					columnsresize : true,
+					columnsreorder : true,
+					columnsresize : true,
+					//rowsheight : 45,
+					autorowheight: true,
+					columns : [
+							{
+								text : '',
+								datafield : 'groupid',
+								width : '10%',
+								cellsrenderer : function(row, column, value) {
+									return '<input type ="button" value="Edit" onClick = "updateProduct('
+											+ value
+											+ ')"></input>'
+											+ '<input type ="button" value="Delete" onClick = "deleteProduct('
+											+ value + ')"></input>';
+								}
+							}, {
+								text : '#',
+								datafield : 'stt',
+								width : '5%',
+								columntype : 'number',
+								cellsrenderer : function(row, column, value) {
+									return row + 1;
+								}
+							}, {
+								text : 'Role Name',
+								datafield : 'groupName',
+								width : '10%'
+							}, {
+								text : 'Description',
+								datafield : 'description',
+								align : 'right',
+								cellsalign : 'right',
+								cellsformat : 'c0',
+								width : '20%'
+							},{
+								text : 'Admin',
+								datafield : 'loginadministrator',
+								align : 'right',
+								cellsalign : 'right',
+								columntype: 'checkbox',
+								width : '9%'
+							},{
+								text : 'View User',
+								datafield : 'viewuser',
+								align : 'right',
+								cellsalign : 'right',
+								columntype: 'checkbox',
+								width : '9%'
+							},{
+								text : 'Add User',
+								datafield : 'adduser',
+								align : 'right',
+								cellsalign : 'right',
+								columntype: 'checkbox',
+								width : '9%'
+							},{
+								text : 'Update User',
+								datafield : 'edituser',
+								align : 'right',
+								cellsalign : 'right',
+								columntype: 'checkbox',
+								width : '9%'
+							},{
+								text : 'View Group',
+								datafield : 'viewgroup',
+								align : 'right',
+								cellsalign : 'right',
+								columntype: 'checkbox',
+								width : '9%'
+							},{
+								text : 'Add Group',
+								datafield : 'addgroup',
+								align : 'right',
+								cellsalign : 'right',
+								columntype: 'checkbox',
+								width : '9%'
+							},{
+								text : 'Update Group',
+								datafield : 'editgroup',
+								align : 'right',
+								cellsalign : 'right',
+								columntype: 'checkbox',
+								width : '9%'
+							}
+							]
+				});
+function updateProduct(userid){
+	window.location.href='/admin/roles/' + userid + '?form';
+}				
