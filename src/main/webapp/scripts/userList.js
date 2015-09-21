481bfdbbc018550274f9598cd730c2ac3792aa4d
luan.vominh <luan.vominh@cosa062.ctivn.com>
2015-09-21 15:01:53 +0700

    update for button in grid

--- src/main/webapp/scripts/userList.js
+++ src/main/webapp/scripts/userList.js
@@ -20,17 +20,21 @@ var source = {
 	}, {
 		name : 'mobile',
 		type : 'string'
-	}	],
+	} ],
 	id : 'userid',
 	url : url
 };
-var cellsrenderer = function(row, columnfield, value, defaulthtml, columnproperties, rowdata) {
-    if (value < 20) {
-        return '<span style="margin: 4px; float: ' + columnproperties.cellsalign + '; color: #ff0000;">' + value + '</span>';
-    }
-    else {
-        return '<span style="margin: 4px; float: ' + columnproperties.cellsalign + '; color: #008000;">' + value + '</span>';
-    }
+var cellsrenderer = function(row, columnfield, value, defaulthtml,
+		columnproperties, rowdata) {
+	if (value < 20) {
+		return '<span style="margin: 4px; float: '
+				+ columnproperties.cellsalign + '; color: #ff0000;">' + value
+				+ '</span>';
+	} else {
+		return '<span style="margin: 4px; float: '
+				+ columnproperties.cellsalign + '; color: #008000;">' + value
+				+ '</span>';
+	}
 }
 var dataAdapter = new $.jqx.dataAdapter(source, {
 	downloadComplete : function(data, status, xhr) {
@@ -54,24 +58,29 @@ $("#list")
 					autosavestate : false,
 					columnsresize : true,
 					columnsreorder : true,
-					//showfilterrow : true,
-					//filterable : true,
+					// showfilterrow : true,
+					// filterable : true,
 					columnsresize : true,
 					rowsheight : 45,
-					//autorowheight: true,
+					// autorowheight: true,
 					columns : [
 							{
 								text : '',
 								datafield : 'userid',
-								width : '15%',
+								width : '20%',
 								cellsrenderer : function(row, column, value) {
-									return '<input type ="button" value="Edit" onClick = "updateProduct('
+									return '<div class="col-md-6">'
+											+ '<a class="btn btn-app" onclick="updateProduct('
 											+ value
-											+ ')"></input>'
-											+ '<input type ="button" value="Delete" onClick = "deleteProduct('
-											+ value + ')"></input>';
+											+ ')">'
+											+ '<i class="glyphicon glyphicon-edit"></i>'
+											+ '</a>'
+											+ '<a class="btn btn-app">'
+											+ '<i class="glyphicon glyphicon-remove-circle"></i>'
+											+ '</a>' + '</div>';
 								}
-							}, {
+							},
+							{
 								text : '#',
 								datafield : 'stt',
 								width : '5%',
@@ -79,18 +88,21 @@ $("#list")
 								cellsrenderer : function(row, column, value) {
 									return row + 1;
 								}
-							}, {
+							},
+							{
 								text : 'User Name',
 								datafield : 'username',
 								width : '10%'
-							}, {
+							},
+							{
 								text : 'Email',
 								datafield : 'email',
 								align : 'right',
 								cellsalign : 'right',
 								cellsformat : 'c0',
 								width : '40%'
-							},{
+							},
+							{
 								text : 'Mobile',
 								datafield : 'mobile',
 								align : 'right',
@@ -98,15 +110,19 @@ $("#list")
 								cellsformat : 'c0',
 								width : '20%'
 							},
-							{text: 'Avatar', datafield: 'avatar', width: '10%',
-								cellsrenderer: function(row, column, value) {
-								
-									if (value)
-										return '<img width="45px" height="45px" src="../../images/profile/' + value + '"/>';
+							{
+								text : 'Avatar',
+								datafield : 'avatar',
+								width : '10%',
+								cellsrenderer : function(row, column, value) {
+
+									if (value) {
+										return '<img width="45px" height="45px" src="../../images/profile/'
+												+ value + '"/>';
+									}
 								}
-							},
-							]
+							}, ]
 				});
-function updateProduct(userid){
-	window.location.href='/admin/users/' + userid + '?form';
-}				
+function updateProduct(userid) {
+	window.location.href = '/admin/users/' + userid + '?form';
+}
