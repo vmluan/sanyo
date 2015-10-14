c34e3bb6a646b30e58a26be3b4dac5625788caa6
luan.vominh <luan.vominh@cosa062.ctivn.com>
2015-10-14 17:31:36 +0700

    update quotation.

--- src/main/webapp/scripts/addQuotation.js
+++ src/main/webapp/scripts/addQuotation.js
@@ -79,7 +79,15 @@ var source = {
 	{
 		name : 'productCode',
 		map : 'product>productCode'
-	},	
+	},
+	{
+		name : 'locationId',
+		map : 'location>locationId'
+	},
+	{
+		name : 'locationName',
+		map : 'location>locationName'
+	},
 	{
 		name : 'orderNo',
 		type : 'string'
@@ -204,15 +212,131 @@ var dataAdapter = new $.jqx.dataAdapter(source, {
 	}
 });
 
+
+
+var source2 = {
+	datatype : "json",
+	datafields : [ 
+	{
+		name : 'productName',
+		map : 'product>productName'
+	},
+	{
+		name : 'productCode',
+		map : 'product>productCode'
+	},
+	{
+		name : 'locationId',
+		map : 'location>locationId'
+	},
+	{
+		name : 'locationName',
+		map : 'location>locationName'
+	},
+	{
+		name : 'orderNo',
+		type : 'string'
+	}, {
+		name : 'unitRate',
+		type : 'string'
+	}, {
+		name : 'quantity',
+		type : 'string'
+	}, {
+		name : 'encounterID',
+		type : 'string'
+	}, {
+		name : 'actualQuantity',
+		type : 'string'
+	}, {
+		name : 'amount',
+		type : 'string'
+
+	}, {
+		name : 'remark',
+		type : 'string'
+
+	}, {
+		name : 'mat_w_o_Tax_USD',
+		type : 'string'
+
+	}, {
+		name : 'mat_w_o_Tax_VND',
+		type : 'string'
+
+	}, {
+		name : 'labour',
+		type : 'string'
+
+	}, {
+		name : 'imp_Tax',
+		type : 'string'
+
+	}, {
+		name : 'special_Con_Tax',
+		type : 'string'
+
+	}, {
+		name : 'discount_rate',
+		type : 'string'
+
+	}, {
+		name : 'encounterTime',
+		type : 'string'
+
+	}, {
+		name : 'vat',
+		type : 'string'
+
+	}, {
+		name : 'unit_Price_After_Discount',
+		type : 'string'
+
+	}, {
+		name : 'allowance',
+		type : 'string'
+
+	}, {
+		name : 'unit_Price_W_Tax_Profit',
+		type : 'string'
+
+	}, {
+		name : 'subcon_Profit',
+		type : 'string'
+
+	}, {
+		name : 'unit_Price_W_Tax_Labour',
+		type : 'string'
+
+	}, {
+		name : 'cost_Mat_Amount_USD',
+		type : 'string'
+
+	}, {
+		name : 'cost_Labour_Amount_USD',
+		type : 'string'
+
+	} ],
+	id : 'encounterID',
+	url : "/test"
+};
+var dataAdapter2 = new $.jqx.dataAdapter(source2, {
+	downloadComplete : function(data, status, xhr) {
+	},
+	loadComplete : function(data) {
+	},
+	loadError : function(xhr, status, error) {
+	}
+});
 // initialize jqxGrid
 $("#list")
 		.jqxGrid(
 				{
 					width : '100%',
-					height : 300,
+					height : 100,
 					theme : 'energyblue',
 					rowsheight : 45,
-					source : dataAdapter,
+					source : dataAdapter2,
 					ready : function() {
 						$('#list').jqxGrid('addrow', null, {}, 'first');
 					},
@@ -522,3 +646,297 @@ function saveEncounter(row) {
 	});
 
 }
+            var toThemeProperty = function (className) {
+                return className + " " + className + "-" + theme;
+            }
+
+            var groupsrenderer = function (text, group, expanded, data) {
+                if (data.groupcolumn.datafield == 'price' || data.groupcolumn.datafield == 'quantity') {
+                    var number = dataAdapter.formatNumber(group, data.groupcolumn.cellsformat);
+                    var text = data.groupcolumn.text + ': ' + number;
+                    if (data.subItems.length > 0) {
+                        var aggregate = this.getcolumnaggregateddata(data.groupcolumn.datafield, ['sum'], true, data.subItems);
+                    }
+                    else {
+                        var rows = new Array();
+                        var getRows = function (group, rows) {
+                            if (group.subGroups.length > 0) {
+                                for (var i = 0; i < group.subGroups.length; i++) {
+                                    getRows(group.subGroups[i], rows);
+                                }
+                            }
+                            else {
+                                for (var i = 0; i < group.subItems.length; i++) {
+                                    rows.push(group.subItems[i]);
+                                }
+                            }
+                        }
+                        getRows(data, rows);
+                        var aggregate = this.getcolumnaggregateddata(data.groupcolumn.datafield, ['sum'], true, rows);
+                    }
+                    
+                    return '<div class="' + toThemeProperty('jqx-grid-groups-row') + '" style="position: absolute;"><span>' + text + ', </span>' + '<span class="' + toThemeProperty('jqx-grid-groups-row-details') + '">' + "Total" + ' (' + aggregate.sum + ')' + '</span></div>';
+                }
+                else {
+                    return '<div class="' + toThemeProperty('jqx-grid-groups-row') + '" style="position: absolute;"><span>' + text + '</span>';
+                }
+            }
+			
+$("#listResult")
+.jqxGrid(
+		{
+			width : '100%',
+			height : 300,
+			theme : 'energyblue',
+			rowsheight : 45,
+			source : dataAdapter,
+			filterable : true,
+			editable : true,
+			groupable: true,
+			groupsrenderer: groupsrenderer,
+			// autorowheight: true,
+			columns : [
+					{
+						text : 'Location',
+						datafield : 'locationName',
+						align : 'center',
+						cellsalign : 'right',
+						width : '15%'
+					},					
+					{
+						text : 'Search by Name',
+						datafield : 'productName',
+						align : 'center',
+						cellsalign : 'right',
+						// cellsformat : 'c0',
+						width : '15%',
+						columntype : 'combobox',
+						createeditor : function(row, column, editor) {
+							// assign a new data source to the combobox.
+							editor.jqxComboBox({
+								autoDropDownHeight : true,
+								source : dataAdapterProducts,
+								displayMember : "productName",
+								valueMember : "productID",
+								promptText : "Please Choose:"
+							});
+
+						},
+						geteditorvalue : function(row, cellvalue,
+								editor) {
+							// return the editor's value.
+							var item = editor
+									.jqxComboBox('getSelectedItem');
+							if (item) {
+								return item.label;
+							}
+						}
+					},
+					{
+						text : 'Search by Code',
+						datafield : 'productCode',
+						align : 'center',
+						cellsalign : 'right',
+						// cellsformat : 'c0',
+						width : '15%',
+						columntype : 'combobox',
+						createeditor : function(row, column, editor) {
+							// assign a new data source to the combobox.
+							editor.jqxComboBox({
+								autoDropDownHeight : true,
+								source : dataAdapterProducts,
+								displayMember : "productCode",
+								valueMember : "productID",
+								promptText : "Please Choose:"
+							});
+						},
+						geteditorvalue : function(row, cellvalue,
+								editor) {
+							// return the editor's value.
+							var item = editor
+									.jqxComboBox('getSelectedItem');
+							if (item) {
+								return item.label;
+							}
+						}
+
+					},
+					{
+						text : 'No',
+						datafield : 'order',
+						align : 'center',
+						cellsalign : 'right',
+						cellsformat : 'c0',
+						width : '15%'
+					},
+					{
+						text : 'Unit',
+						datafield : 'unit',
+						align : 'center',
+						cellsalign : 'right',
+						// cellsformat : 'c0',
+						width : '15%'
+					},
+					{
+						text : 'Quantity',
+						datafield : 'quantity',
+						align : 'center',
+						cellsalign : 'right',
+						// cellsformat : 'c0',
+						width : '15%'
+					},
+					{
+						text : 'Unit Rate',
+						datafield : 'unitRate',
+						align : 'center',
+						cellsalign : 'right',
+						// cellsformat : 'c0',
+						width : '15%'
+					},
+					{
+						text : 'Amount',
+						datafield : 'amount',
+						align : 'center',
+						cellsalign : 'right',
+						// cellsformat : 'c0',
+						width : '15%'
+					},
+					{
+						text : 'Remark',
+						datafield : 'remark',
+						align : 'center',
+						cellsalign : 'right',
+						// cellsformat : 'c0',
+						width : '15%'
+					},
+					{
+						text : 'Labour',
+						datafield : 'labour',
+						align : 'center',
+						cellsalign : 'right',
+						// cellsformat : 'c0',
+						width : '15%'
+					},
+					{
+						text : 'Mat w/o Tax USD',
+						datafield : 'mat_w_o_Tax_USD',
+						align : 'center',
+						cellsalign : 'right',
+						// cellsformat : 'c0',
+						width : '15%'
+					},
+					{
+						text : 'Mat w/o Tax VND',
+						datafield : 'mat_w_o_Tax_VND',
+						align : 'center',
+						cellsalign : 'right',
+						// cellsformat : 'c0',
+						width : '15%'
+					},
+					{
+						text : 'Imp Tax',
+						datafield : 'imp_Tax',
+						align : 'center',
+						cellsalign : 'right',
+						// cellsformat : 'c0',
+						width : '15%'
+					},
+					{
+						text : 'Special con. Tax',
+						datafield : 'special_Con_Tax',
+						align : 'center',
+						cellsalign : 'right',
+						// cellsformat : 'c0',
+						width : '15%'
+					},
+					{
+						text : 'VAT',
+						datafield : 'vat',
+						align : 'center',
+						cellsalign : 'right',
+						// cellsformat : 'c0',
+						width : '15%'
+					},
+					{
+						text : 'Discount rate %',
+						datafield : 'discount_rate',
+						align : 'center',
+						cellsalign : 'right',
+						// cellsformat : 'c0',
+						width : '15%'
+					},
+					{
+						text : 'Unit price after discount',
+						datafield : 'unit_Price_After_Discount',
+						align : 'center',
+						cellsalign : 'right',
+						// cellsformat : 'c0',
+						width : '15%'
+					},
+					{
+						text : 'Allowance',
+						datafield : 'allowance',
+						align : 'center',
+						cellsalign : 'right',
+						// cellsformat : 'c0',
+						width : '15%'
+					},
+					{
+						text : 'Unit price w Tax & profit',
+						datafield : 'unit_Price_W_Tax_Profit',
+						align : 'center',
+						cellsalign : 'right',
+						// cellsformat : 'c0',
+						width : '15%'
+					},
+					{
+						text : 'Subcon profit',
+						datafield : 'subConProfit',
+						align : 'center',
+						cellsalign : 'right',
+						// cellsformat : 'c0',
+						width : '15%'
+					},
+					{
+						text : 'Unit price w Tax – labour',
+						datafield : 'unit_Price_W_Tax_Labour',
+						align : 'center',
+						cellsalign : 'right',
+						// cellsformat : 'c0',
+						width : '15%'
+					},
+					{
+						text : 'Cost - Mat amount USD',
+						datafield : 'cost_Mat_Amount_USD',
+						align : 'center',
+						cellsalign : 'right',
+						// cellsformat : 'c0',
+						width : '10%'
+					},
+					{
+						text : 'Cost - Labour amount USD',
+						datafield : 'cost_Labour_Amount_USD',
+						align : 'center',
+						cellsalign : 'right',
+						// cellsformat : 'c0',
+						width : '10%'
+					},
+					{
+						text : 'Action',
+						align : 'center',
+						datafield : '',
+						width : '10%',
+						cellsrenderer : function(row, column, value) {
+							return '<div class="col-md-6">'
+									+ '<a class="btn btn-app" onclick="addItem('
+									+ row
+									+ ')">'
+									+ '<i class="glyphicon glyphicon-plus"></i>'
+									+ '</div>';
+						},
+						cellbeginedit : function(row) {
+							return false;
+						}
+					} ],
+					groups: ['locationName']
+		});
