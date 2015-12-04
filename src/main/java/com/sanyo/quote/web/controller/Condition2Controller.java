ebc3ad34c80c28f50a019d56af8c7bd9ba5edfba
vanduocvn87 <vanduocvn87@gmail.com>
2015-12-04 17:12:16 +0700

    Update Conditon2 Form

--- src/main/java/com/sanyo/quote/web/controller/Condition2Controller.java
+++ src/main/java/com/sanyo/quote/web/controller/Condition2Controller.java
@@ -1,9 +1,18 @@
 package com.sanyo.quote.web.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.sanyo.quote.domain.Project;
+import com.sanyo.quote.service.ProjectService;
 
 /**
  * Created by Chuong Thai on 10/8/2015.
@@ -11,10 +20,22 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 @RequestMapping(value = "/condition_2")
-public class Condition2Controller {
+public class Condition2Controller extends CommonController {
+	final Logger logger = LoggerFactory.getLogger(CoverController.class);
+	
+	@Autowired
+	private ProjectService projectService;
 
-    @RequestMapping(method = RequestMethod.GET)
+    /*@RequestMapping(method = RequestMethod.GET)
     public String getBOQPage(Model uiModel) {
         return "quotation/condition_2";
-    }
+    }*/
+    
+    @RequestMapping(method = RequestMethod.GET)
+	public String getCoverPage(@RequestParam(value="projectId", required=true) String projectId,
+			Model uiModel,HttpServletRequest httpServletRequest) {
+		Project project = projectService.findById(Integer.valueOf(projectId));
+		uiModel.addAttribute("project", project);		
+		return "quotation/condition_2";
+	}
 }
