e798a8b651bb122bf53bc27f40c4424a5b65f2d0
vanduocvn87 <vanduocvn87@gmail.com>
2015-12-28 00:14:02 +0700

    Commit bổ sung

---
+++ src/main/java/com/sanyo/quote/web/controller/convertDateToString.java
@@ -0,0 +1,22 @@
+package com.sanyo.quote.web.controller;
+
+public class convertDateToString {
+	private String strDate;
+	private String revision_no;
+	public String getStrDate()
+	{
+		return strDate;
+	}
+	public void setStrDate(String strDate)
+	{
+		this.strDate = strDate;
+	}
+	public String getRevision_no()
+	{
+		return revision_no;
+	}
+	public void setRevision_no(String revision_no)
+	{
+		this.revision_no = revision_no;
+	}
+}
