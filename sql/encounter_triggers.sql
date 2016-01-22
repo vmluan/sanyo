ba51dcdeec8710ec960f248969f2db22a8cd0864
hiepkhachbk <thaichuong159@gmail.com>
2016-01-22 19:43:12 +0700

    update trigger for labour

--- sql/encounter_triggers.sql
+++ sql/encounter_triggers.sql
@@ -8,6 +8,7 @@ DROP TRIGGER IF EXISTS encounterAddTrigger;
 		BEGIN
 			declare v_max_orderNo int(11);
 			declare encounter_total_material FLOAT;
+			declare encounter_total_labour FLOAT;
 			declare v_project_id int(11);
 			declare v_product_group_id int(11);
 
@@ -21,16 +22,16 @@ DROP TRIGGER IF EXISTS encounterAddTrigger;
             end if;
 			set NEW.order_No = v_max_orderNo +1;
 
-			-- Update total material
+			-- Update total material and total labour
 			select project_id into v_project_id from location l where l.LOCATION_ID in (SELECT LOCATION_ID from region where REGION_ID=NEW.REGION_ID);
 			select product_group_id into v_product_group_id from product p where p.PRODUCT_ID in (SELECT PRODUCT_ID from encounter where PRODUCT_ID=NEW.PRODUCT_ID);
 
-			select sum(Cost_Mat_Amount_USD) into encounter_total_material
+			select sum(Cost_Mat_Amount_USD),sum(Cost_Labour_Amount_USD) into encounter_total_material, encounter_total_labour
 			FROM encounter
 			WHERE PRODUCT_ID in (select PRODUCT_ID from product where product_group_id=v_product_group_id) AND
 			REGION_ID in (select REGION_ID from region r where r.LOCATION_ID in (select LOCATION_ID from location l where l.PROJECT_ID=v_project_id ));
 			update productgrouprate
-			SET total_material = encounter_total_material
+			SET total_material = encounter_total_material, total_labor=encounter_total_labour
 			WHERE  product_group_id = v_product_group_id AND PROJECT_ID=v_project_id;
 		END;
 
