1d425be4f30878ff7e2af3c2ef2053a69f5f2967
phanduong2211 <phanduong2211@gmail.com>
2016-03-28 17:15:24 +0700

    getDate revigion for condition1
    getDate revigion  for condition1

--- src/main/java/com/sanyo/quote/web/controller/Condition1Controller.java
+++ src/main/java/com/sanyo/quote/web/controller/Condition1Controller.java
@@ -1,6 +1,8 @@
 package com.sanyo.quote.web.controller;
 
+import java.text.DateFormat;
 import java.util.Date;
+import java.util.Locale;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -13,7 +15,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.List;
+
 import com.sanyo.quote.domain.Project;
+import com.sanyo.quote.domain.ProjectRevision;
+import com.sanyo.quote.service.ProjectRevisionService;
 import com.sanyo.quote.service.ProjectService;
 
 /**
@@ -27,7 +33,8 @@ public class Condition1Controller extends CommonController {
 
 	@Autowired
 	private ProjectService projectService;
-	
+	@Autowired
+	private ProjectRevisionService projectRevision; 
     /*@RequestMapping(method = RequestMethod.GET)
     public String getBOQPage(Model uiModel) {
         return "quotation/condition_1";
@@ -37,6 +44,21 @@ public class Condition1Controller extends CommonController {
 	public String getCoverPage(@RequestParam(value="projectId", required=true) String projectId,
 			Model uiModel,HttpServletRequest httpServletRequest) {
 		Project project = projectService.findById(Integer.valueOf(projectId));
+		List<ProjectRevision> projectrevigion = projectRevision.findRevisions(project);
+		Date maxDay = null;
+		for(ProjectRevision itemRevigion:projectrevigion)
+		{
+			if(maxDay==null)
+				maxDay = itemRevigion.getDate();
+			else
+			{
+				if(maxDay.compareTo(itemRevigion.getDate())<0)
+				{
+					maxDay = itemRevigion.getDate();
+				}
+			}
+		}
+		
 		Date startDate=project.getStartDate();
 		Date endate = project.getEndDate();
 		
@@ -48,6 +70,24 @@ public class Condition1Controller extends CommonController {
 		uiModel.addAttribute("summonth", sumMonth);
 		uiModel.addAttribute("sumDay", sumDay);
 		uiModel.addAttribute("CountWeek", CountWeek);
+		uiModel.addAttribute("Escalation",convertDate(maxDay));
+		
 		return "quotation/condition_1";
 	}
+    private String convertDate(Date date)
+    {
+    	//Date today;
+    	if(date==null)
+    		return "null";
+    	String dateOut;
+    	DateFormat dateFormatter;
+    	Locale currentLocale = Locale.US;
+    	
+    	dateFormatter = DateFormat.getDateInstance(DateFormat.MONTH_FIELD, currentLocale);
+    	//today = new Date();
+    	dateOut = dateFormatter.format(date);
+    	String month = dateOut.split(" ")[0];
+    	String year = dateOut.split(" ")[2];
+    	return month+" "+year;
+    }
 }
