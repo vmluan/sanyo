b80629ec723dde9eb27451a1a79951e820ecdc77
chuong <thaichuong150@gmail.com>
2016-08-11 01:10:26 +0700

    Updated main menu

--- src/main/webapp/scripts/projectList.js
+++ src/main/webapp/scripts/projectList.js
@@ -166,8 +166,8 @@ $("#list")
 									var result = 
 												'<div class="col-md-12">'
 												+'<p>'
-													+ '<button style="float:none;margin:6px" class="btn bg-olive margin"  onclick="makeReport('+ value +  ')"' + '>Print</button>'
-													+ '<button style="float:none;margin:6px" class="btn bg-olive margin"  onclick="makeReport('+ value + ',' + lang + ')"' + '>Print VN</button>'
+													+ '<button style="float:none;margin:6px" class="btn bg-olive margin"  onclick="makeReport('+ value +  ')"' + '>Print (En)</button>'
+													+ '<button style="float:none;margin:6px" class="btn bg-olive margin"  onclick="makeReport('+ value + ',' + lang + ')"' + '>Print (VN)</button>'
 													+ '<button style="float:none;margin:6px" class="btn bg-olive margin"  onclick="updateProduct('+ value +  ')"' + '>Basic Info</button>'
 													+ '<button style="float:none;margin:6px" class="btn bg-purple margin" onclick="addQuotation('+ value +  ')"' + '>Quotation</button>'
 													+ '<button style="float:none;margin:6px" class="btn bg-navy margin">Marker List</button>'
